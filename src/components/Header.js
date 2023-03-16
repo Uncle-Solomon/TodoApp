de1f9670807bfdd940162757996edc3113e021eb
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-16 13:28:41 +0100

    End of lesson 38 - Adding Header

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../style.module.css";
+
+const Header = () => {
+  return <div className={styles.header}>Todo App</div>;
+};
+
+export default Header;
