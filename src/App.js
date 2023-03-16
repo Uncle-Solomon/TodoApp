de7c4eb6bd9d689473db944a8d97b35f7f95ed8e
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-16 15:08:52 +0100

    End of lesson 40 - Adding Items to Todo List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "../style.module.css";
 
-const Form = () => {
+const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoList([...todoList, todo]);
+  };
   return (
     <div className={styles.todoform}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
+          value={todo}
           className={styles.todoinput}
           placeholder="Add Todo Item"
           type="text"
